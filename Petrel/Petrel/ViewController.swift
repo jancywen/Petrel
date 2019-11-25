af0dbe24df6f7a471bc1638dcf06823cd5db2ba7
wangwenjie <jancywen@163.com>
2019-11-25 13:25:41 +0800

    Initialization engineering

---
+++ Petrel/Petrel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Petrel
+//
+//  Created by wangwenjie on 2019/11/25.
+//  Copyright © 2019 Petrel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
